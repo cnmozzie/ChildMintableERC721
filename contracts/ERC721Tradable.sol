ff3bbfe29c916a15d0318c18974d604c9a98a59d
mozzie <mozzie@localhost.localdomain>
2021-07-27 12:24:08 +0800

    implement the contracts

---
+++ contracts/ERC721Tradable.sol
@@ -0,0 +1,267 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/math/SafeMath.sol";
+import "@openzeppelin/contracts/utils/Strings.sol";
+
+import "./common/meta-transactions/ContentMixin.sol";
+import "./common/meta-transactions/NativeMetaTransaction.sol";
+import "./IChildToken.sol";
+import "./common/AccessControlMixin.sol";
+
+contract OwnableDelegateProxy {}
+
+contract ProxyRegistry {
+    mapping(address => OwnableDelegateProxy) public proxies;
+}
+
+/**
+ * @title ERC721Tradable
+ * ERC721Tradable - ERC721 contract that whitelists a trading address, and has minting functionality.
+ */
+abstract contract ERC721Tradable is 
+    ContextMixin, 
+    AccessControlMixin, 
+    ERC721Enumerable, 
+    IChildToken,
+    NativeMetaTransaction, 
+    Ownable 
+{
+    using SafeMath for uint256;
+    
+    bytes32 public constant DEPOSITOR_ROLE = keccak256("DEPOSITOR_ROLE");
+    bytes32 public constant RENT_ROLE = keccak256("RENT_ROLE");
+    mapping (uint256 => uint256) private rentDueTime;
+    mapping (uint256 => address) private _holderOf;
+    mapping (uint256 => bool) public withdrawnTokens;
+    
+    mapping (uint => string) private _tokenURI;
+
+    // limit batching of tokens due to gas limit restrictions
+    uint256 public constant BATCH_LIMIT = 20;
+
+    event WithdrawnBatch(address indexed user, uint256[] tokenIds);
+    event TransferWithMetadata(address indexed from, address indexed to, uint256 indexed tokenId, bytes metaData);
+
+    address proxyRegistryAddress;
+    uint256 private _currentTokenId = 0;
+
+    constructor(
+        string memory _name,
+        string memory _symbol,
+        address _proxyRegistryAddress,
+        address childChainManager
+    ) ERC721(_name, _symbol) {
+        _setupContractId("ChildMintableERC721");
+        proxyRegistryAddress = _proxyRegistryAddress;
+        _setupRole(DEFAULT_ADMIN_ROLE, _msgSender());
+        _setupRole(DEPOSITOR_ROLE, childChainManager);
+        _initializeEIP712(_name);
+    }
+    
+    /**
+     * @notice called when token is deposited on root chain
+     * @dev Should be callable only by ChildChainManager
+     * Should handle deposit by minting the required tokenId(s) for user
+     * Should set `withdrawnTokens` mapping to `false` for the tokenId being deposited
+     * Minting can also be done by other functions
+     * @param user user address for whom deposit is being done
+     * @param depositData abi encoded tokenIds. Batch deposit also supported.
+     */
+    function deposit(address user, bytes calldata depositData)
+        external
+        override
+        only(DEPOSITOR_ROLE)
+    {
+
+        // deposit single
+        if (depositData.length == 32) {
+            uint256 tokenId = abi.decode(depositData, (uint256));
+            withdrawnTokens[tokenId] = false;
+            _mint(user, tokenId);
+
+        // deposit batch
+        } else {
+            uint256[] memory tokenIds = abi.decode(depositData, (uint256[]));
+            uint256 length = tokenIds.length;
+            for (uint256 i; i < length; i++) {
+                withdrawnTokens[tokenIds[i]] = false;
+                _mint(user, tokenIds[i]);
+            }
+        }
+
+    }
+    
+    /**
+     * @notice called when user wants to withdraw token back to root chain
+     * @dev Should handle withraw by burning user's token.
+     * Should set `withdrawnTokens` mapping to `true` for the tokenId being withdrawn
+     * This transaction will be verified when exiting on root chain
+     * @param tokenId tokenId to withdraw
+     */
+    function withdraw(uint256 tokenId) external {
+        require(_msgSender() == ownerOf(tokenId), "ChildMintableERC721: INVALID_TOKEN_OWNER");
+        withdrawnTokens[tokenId] = true;
+        _burn(tokenId);
+    }
+
+    /**
+     * @notice called when user wants to withdraw multiple tokens back to root chain
+     * @dev Should burn user's tokens. This transaction will be verified when exiting on root chain
+     * @param tokenIds tokenId list to withdraw
+     */
+    function withdrawBatch(uint256[] calldata tokenIds) external {
+
+        uint256 length = tokenIds.length;
+        require(length <= BATCH_LIMIT, "ChildMintableERC721: EXCEEDS_BATCH_LIMIT");
+
+        // Iteratively burn ERC721 tokens, for performing
+        // batch withdraw
+        for (uint256 i; i < length; i++) {
+
+            uint256 tokenId = tokenIds[i];
+
+            require(_msgSender() == ownerOf(tokenId), string(abi.encodePacked("ChildMintableERC721: INVALID_TOKEN_OWNER ", tokenId)));
+            withdrawnTokens[tokenId] = true;
+            _burn(tokenId);
+
+        }
+
+        // At last emit this event, which will be used
+        // in MintableERC721 predicate contract on L1
+        // while verifying burn proof
+        emit WithdrawnBatch(_msgSender(), tokenIds);
+
+    }
+
+    /**
+     * @notice called when user wants to withdraw token back to root chain with token URI
+     * @dev Should handle withraw by burning user's token.
+     * Should set `withdrawnTokens` mapping to `true` for the tokenId being withdrawn
+     * This transaction will be verified when exiting on root chain
+     *
+     * @param tokenId tokenId to withdraw
+     */
+    function withdrawWithMetadata(uint256 tokenId) external {
+
+        require(_msgSender() == ownerOf(tokenId), "ChildMintableERC721: INVALID_TOKEN_OWNER");
+        withdrawnTokens[tokenId] = true;
+
+        // Encoding metadata associated with tokenId & emitting event
+        emit TransferWithMetadata(ownerOf(tokenId), address(0), tokenId, this.encodeTokenMetadata(tokenId));
+
+        _burn(tokenId);
+
+    }
+
+    /**
+     * @notice This method is supposed to be called by client when withdrawing token with metadata
+     * and pass return value of this function as second paramter of `withdrawWithMetadata` method
+     *
+     * It can be overridden by clients to encode data in a different form, which needs to
+     * be decoded back by them correctly during exiting
+     *
+     * @param tokenId Token for which URI to be fetched
+     */
+    function encodeTokenMetadata(uint256 tokenId) external view virtual returns (bytes memory) {
+
+        // You're always free to change this default implementation
+        // and pack more data in byte array which can be decoded back
+        // in L1
+        return abi.encode(tokenURI(tokenId));
+
+    }
+
+    /**
+     * @dev Mints a token to an address with a tokenURI.
+     * @param _to address of the future owner of the token
+     */
+    function mintTo(address _to) public onlyOwner {
+        uint256 newTokenId = _getNextTokenId();
+        _mint(_to, newTokenId);
+        _incrementTokenId();
+    }
+
+    /**
+     * @dev calculates the next token ID based on value of _currentTokenId
+     * @return uint256 for the next token ID
+     */
+    function _getNextTokenId() private view returns (uint256) {
+        return _currentTokenId.add(1);
+    }
+
+    /**
+     * @dev increments the value of _currentTokenId
+     */
+    function _incrementTokenId() private {
+        _currentTokenId++;
+    }
+
+    function baseTokenURI() virtual public pure returns (string memory);
+
+    function tokenURI(uint256 _tokenId) override public view returns (string memory) {
+        bytes memory tempURI = bytes(_tokenURI[_tokenId]);
+        if (tempURI.length == 0) {
+          return string(abi.encodePacked(baseTokenURI(), Strings.toString(_tokenId)));
+        }
+        else {
+          return _tokenURI[_tokenId];
+        }
+    }
+    
+    function setTokenURI(uint256 tokenId, string memory uri) public only(DEFAULT_ADMIN_ROLE) {
+        _tokenURI[tokenId] = uri;
+    }
+    
+    function setHolder(uint256 tokenId, address newHolder, uint256 rentDay) public only(RENT_ROLE) {
+        _holderOf[tokenId] = newHolder;
+        rentDueTime[tokenId] = block.timestamp + rentDay * 1 days;
+    }
+    
+    function holderOf(uint256 tokenId) public view returns (address) {
+        if (block.timestamp > rentDueTime[tokenId]) {
+          return ownerOf(tokenId);
+        }
+        else {
+          return _holderOf[tokenId];
+        }
+    }
+
+    /**
+     * Override isApprovedForAll to whitelist user's OpenSea proxy accounts to enable gas-less listings.
+     */
+    function isApprovedForAll(address owner, address operator)
+        override
+        public
+        view
+        returns (bool)
+    {
+        // Whitelist OpenSea proxy contract for easy trading.
+        ProxyRegistry proxyRegistry = ProxyRegistry(proxyRegistryAddress);
+        if (address(proxyRegistry.proxies(owner)) == operator) {
+            return true;
+        }
+
+        return super.isApprovedForAll(owner, operator);
+    }
+
+    /**
+     * This is used instead of msg.sender as transactions won't be sent by the original token owner, but by OpenSea.
+     */
+    function _msgSender()
+        internal
+        override
+        view
+        returns (address sender)
+    {
+        return ContextMixin.msgSender();
+    }
+    
+    function supportsInterface(bytes4 interfaceId) public view virtual override(ERC721Enumerable, AccessControl) returns (bool) {
+        return super.supportsInterface(interfaceId);
+    }
+}
---
+++ migrations/2_deploy_contracts.js
@@ -0,0 +1,9 @@
+const Creature = artifacts.require("Creature");
+
+module.exports = function(deployer) {
+  
+  const proxyRegistryAddress = '0xf57b2c51ded3a29e6891aba85459d600256cf317'; // rinkeby
+  //const proxyRegistryAddress = '0x58807baD0B376efc12F5AD86aAc70E78ed67deaE'; // matic
+  const ChildChainManagerProxy = '0xb5505a6d998549090530911180f38aC5130101c6'; // matic
+  deployer.deploy(Creature, proxyRegistryAddress, ChildChainManagerProxy);
+};
