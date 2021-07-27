de963c4767921a8d3bf1e788857be4e27f5eff81
mozzie <mozzie@localhost.localdomain>
2021-07-27 22:04:36 +0800

    change the contract address

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -2,8 +2,9 @@ const Creature = artifacts.require("Creature");
 
 module.exports = function(deployer) {
   
-  const proxyRegistryAddress = '0xf57b2c51ded3a29e6891aba85459d600256cf317'; // rinkeby
-  //const proxyRegistryAddress = '0x58807baD0B376efc12F5AD86aAc70E78ed67deaE'; // matic
-  const ChildChainManagerProxy = '0xb5505a6d998549090530911180f38aC5130101c6'; // matic
+  //const proxyRegistryAddress = '0xf57b2c51ded3a29e6891aba85459d600256cf317'; // rinkeby
+  const proxyRegistryAddress = '0x58807baD0B376efc12F5AD86aAc70E78ed67deaE'; // matic
+  //const ChildChainManagerProxy = '0xb5505a6d998549090530911180f38aC5130101c6'; // mumbai
+  const ChildChainManagerProxy = '0xA6FA4fB5f76172d178d61B04b0ecd319C5d1C0aa'; // matic
   deployer.deploy(Creature, "LOSERPUNKS", "失败朋克", proxyRegistryAddress, ChildChainManagerProxy);
 };
